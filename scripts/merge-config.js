b6396e2d4522b615a81bf3ecc2df0446bcb79ebf
liuyib <1656081615@qq.com>
2019-05-24 22:28:14 +0800

    chore: set hexo data files

--- scripts/merge-config.js
+++ scripts/merge-config.js
@@ -1,14 +1,19 @@
-const version = require('../package.json').version
+var version = require('../package.json').version;
 
 hexo.extend.helper.register('version', function () {
-  return version
-})
+  return version;
+});
 
-// hexo.on('generateBefore', function () {
-//   const rootConfig = hexo.config
-//   if (hexo.locals.get) {
-//     const data = hexo.locals.get('data')
-//     data && data.melody && (hexo.theme.config = data.melody)
-//   }
-//   hexo.theme.config.rootConfig = rootConfig
-// })
+hexo.on('generateBefore', function () {
+  var rootConfig = hexo.config;
+
+  if (hexo.locals.get) {
+    var data = hexo.locals.get('data');
+    
+    if (data && data.stun) {
+      hexo.theme.config = data.stun;
+    }
+  }
+
+  hexo.theme.config.rootConfig = rootConfig;
+});
