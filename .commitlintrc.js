63a63eb9adac60811e3a0a5fc322adb8b09e043a
liuyib <1656081615@qq.com>
2019-05-24 22:39:56 +0800

    chore: Modify commit rules

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -7,27 +7,19 @@ module.exports = {
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
-    ],
+    'subject-case': [2, 'always', 'sentence-case'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'refactor',
-        'docs',
-        'style',
-        'test',
-        'chore'
-      ]
-    ]
+    'type-enum': [2, 'always', [
+      'feat',
+      'fix',
+      'refactor',
+      'docs',
+      'style',
+      'test',
+      'chore'
+    ]]
   }
 };
