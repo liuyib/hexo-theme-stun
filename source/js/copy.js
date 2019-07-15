4236247f46570d203c8a5c23f57b1b83f5fab8a5
liuyib <1656081615@qq.com>
2019-07-15 21:49:04 +0800

    refactor: Optimized the function & UI of copy code

--- source/js/copy.js
+++ source/js/copy.js
@@ -1,27 +1,48 @@
 $(document).ready(function() {
-  var $copyIcon = $(
-    `<div class="copy-button">
+  var CODEBLOCK_CLASS_NAME = 'highlight';
+  var $codeBlocks = $('figure.highlight');
+  
+  $codeBlocks.each(function(index, item) {
+    if (!$(item).find('figcaption')[0]) {
+      var lang = item.getAttribute('class')
+        .split(/\s/).filter(e => e !== CODEBLOCK_CLASS_NAME);
+      var codeHeader = $(`
+        <figcaption class="custom">
+          <span>${lang}</span>
+        </figcaption>`)[0];
+      
+      item.insertBefore(codeHeader, $(item).find('table')[0]);
+    }
+  });
+
+  var $copyIcon = $(`
+    <div class="copy-button">
       <i class="fa fa-clipboard"></i>
     </div>`);
-  $('figure.highlight').append($copyIcon);
-
-  $('.copy-button i').on('click', function() {
-    if (copy(this)) {
+  
+  $('figure.highlight figcaption').append($copyIcon);
+  $('.copy-button').on('click', function() {
+    var codeContainer = $(this).parent('figcaption')
+        .siblings('table').find('td.code')[0];
+    
+    if (copy(codeContainer)) {
       popAlert('success', '复制成功', 4);
     } else {
       popAlert('error', '复制失败');
     }
   });
 
-  function copy(btn) {
+  /**
+   * Copy code
+   * @param {HTMLElement} container Container of code.
+   */
+  function copy(container) {
     try {
-      var $codeWrapper =
-        $(btn).parent('.copy-button').siblings('table');
       var selection = window.getSelection();
       var range = document.createRange();
       
       // Select text by the content of node.
-      range.selectNodeContents($codeWrapper[0]);
+      range.selectNodeContents(container);
       selection.removeAllRanges();
       selection.addRange(range);
         
