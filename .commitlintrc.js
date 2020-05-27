ac4c157a022acc4d42ca9038c641528adb17c741
liuyib <1656081615@qq.com>
2020-05-27 11:10:34 +0800

    chore: full use of standard style js code

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
