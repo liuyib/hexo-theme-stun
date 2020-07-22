d1a01dd7354ebeff57cb7d79d67529d51b53c6da
liuyib <1656081615@qq.com>
2020-07-22 10:57:24 +0800

    chore: disable case checking for commit

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 72],
     'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [2, 'always', 'lower-case'],
+    'subject-case': [0],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
