1b10df3d2aa07e061d80baabba9dc79fedfc845c
liuyihui <liuyihui02@gmail.com>
2021-12-16 19:44:21 +0800

    Refactor Note tag

--- scripts/tags/note.js
+++ scripts/tags/note.js
@@ -1,40 +1,51 @@
-'use strict'
+/**
+ * note.js | https://github.com/volantis-x/hexo-theme-volantis
+ */
 
-function note (args, content) {
-  var theme = hexo.theme.config
-  var icon = theme.icon && theme.icon.notetag_default
-  var iconType = 'default'
-  var isIcon = true
+ 'use strict';
 
-  if (args.includes('no-icon')) {
-    isIcon = false
-  }
-  if (isIcon && theme.icon) {
-    var tagTypes = ['default', 'success', 'info', 'warning', 'danger']
-    tagTypes.forEach(type => {
-      if (args.includes(type)) {
-        icon = theme.icon[`notetag_${type}`]
-        iconType = type
-      }
-    })
-  }
-
-  var className = args.join(' ')
-  return `
-    <div class="note-plugin ${className}">
-      ${
-        isIcon
-          ? `<span class="note-plugin__icon note-plugin__icon--${iconType}">
-              <i class="${icon}"></i>
-            </span>`
-          : ''
-      }
-      ${hexo.render
-        .renderSync({ text: content, engine: 'markdown' })
-        .split('\n')
-        .join('')}
-    </div>
-  `
-}
-
-hexo.extend.tag.register('note', note, { ends: true })
+ // {% note style, content %}
+ function postNote(args) {
+   if(/::/g.test(args)){
+     args = args.join(' ').split('::');
+   }
+   else{
+     args = args.join(' ').split(',');
+   }
+   if (args.length > 1) {
+     const cls = args[0].trim();
+     const text = args[1].trim();
+     return `<div class="note ${cls}">${hexo.render.renderSync({text: text, engine: 'markdown'}).split('\n').join('')}</div>`;
+   } else if (args.length > 0) {
+     const text = args[0].trim();
+     return `<div class="note">${hexo.render.renderSync({text: text, engine: 'markdown'}).split('\n').join('')}</div>`;
+   }
+ }
+ 
+ // {% noteblock style, title %}
+ // content
+ // {% endnoteblock %}
+ function postNoteBlock(args, content) {
+   if(/::/g.test(args)){
+     args = args.join(' ').split('::');
+   }
+   else{
+     args = args.join(' ').split(',');
+   }
+   if (args.length < 1) {
+     return;
+   }
+   const cls = args[0].trim();
+   let ret = '';
+   ret += '<div class="note ' + cls + '">';
+   if (args.length > 1) {
+     const title = args[1].trim();
+     ret += '<p><strong>' + title + '</strong></p>';
+   }
+   ret += hexo.render.renderSync({text: content, engine: 'markdown'}).split('\n').join('');
+   ret += '</div>';
+   return ret;
+ }
+ 
+ hexo.extend.tag.register('note', postNote);
+ hexo.extend.tag.register('noteblock', postNoteBlock, {ends: true});
